e8784e5d52c97b18243af96d280c684c952c2105
Pison Golda <pison.gm.dev@gmail.com>
2026-03-24 11:53:18 +0700

    fix: rename middleware back to proxy because nextjs 16 (new version)

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// WAJIB bernama 'middleware' agar terdeteksi oleh Next.js
-export function middleware(request: NextRequest) {
+// WAJIB bernama 'proxy' untuk Next.js 16+
+export function proxy(request: NextRequest) {
   const token = request.cookies.get('mosque_session')?.value;
   const isAuthPage = request.nextUrl.pathname === '/';
   const isDashboardPage = request.nextUrl.pathname.startsWith('/dashboard');
 
   if (!token && isDashboardPage) {
-    // Gunakan nextUrl.clone() agar protocol & host-nya akurat
     const url = request.nextUrl.clone();
     url.pathname = '/';
     return NextResponse.redirect(url);
